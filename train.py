c8b2f659e12eb833bfcf6b0ff985d5192391d8e1
Alex Zhuang <alexdayouzhuang@gmail.com>
2023-08-13 20:46:16 -0400

    fix

--- train.py
+++ train.py
@@ -11,7 +11,8 @@
 #    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #    See the License for the specific language governing permissions and
 #    limitations under the License.
-
+import os
+import pickle
 import copy
 import logging
 from dataclasses import dataclass, field
@@ -22,6 +23,7 @@ import transformers
 import utils
 from torch.utils.data import Dataset
 from transformers import Trainer
+from tqdm import tqdm
 
 IGNORE_INDEX = -100
 DEFAULT_PAD_TOKEN = "[PAD]"
@@ -39,6 +41,9 @@ PROMPT_DICT = {
         "Write a response that appropriately completes the request.\n\n"
         "### Instruction:\n{instruction}\n\n### Response:"
     ),
+    "prompt_no_instruction": (
+        "### Input:\n{input}\n\n### Response:"
+    )
 }
 
 
@@ -50,6 +55,8 @@ class ModelArguments:
 @dataclass
 class DataArguments:
     data_path: str = field(default=None, metadata={"help": "Path to the training data."})
+    has_instruction: bool = field(default=True, metadata={"help": "Whether we should use instructions for this dataset."})
+    dataset_type: str = field(default="llama", metadata={"help": "Type of dataset. [llama, skg]"})
 
 
 @dataclass
@@ -127,18 +134,22 @@ def preprocess(
 class SupervisedDataset(Dataset):
     """Dataset for supervised fine-tuning."""
 
-    def __init__(self, data_path: str, tokenizer: transformers.PreTrainedTokenizer):
+    def __init__(self, data_path: str, has_instruction: bool, tokenizer: transformers.PreTrainedTokenizer):
         super(SupervisedDataset, self).__init__()
         logging.warning("Loading data...")
         list_data_dict = utils.jload(data_path)
 
+        # sources is all the input, up to "### Response: " targets is raw output
         logging.warning("Formatting inputs...")
-        prompt_input, prompt_no_input = PROMPT_DICT["prompt_input"], PROMPT_DICT["prompt_no_input"]
-        sources = [
-            prompt_input.format_map(example) if example.get("input", "") != "" else prompt_no_input.format_map(example)
-            for example in list_data_dict
-        ]
-        targets = [f"{example['output']}{tokenizer.eos_token}" for example in list_data_dict]
+        prompt_input, prompt_no_input, prompt_no_instr = PROMPT_DICT["prompt_input"], PROMPT_DICT["prompt_no_input"], PROMPT_DICT["prompt_no_instruction"]
+        sources = []
+        targets = []
+        for example in tqdm(list_data_dict):
+            if has_instruction:
+                sources.append(prompt_input.format_map(example) if example.get("input", "") != "" else prompt_no_input.format_map(example))
+            else:
+                sources.append(prompt_no_instr.format_map(example))
+            targets.append(f"{example['output']}{tokenizer.eos_token}")
 
         logging.warning("Tokenizing inputs... This may take some time...")
         data_dict = preprocess(sources, targets, tokenizer)
@@ -152,6 +163,98 @@ class SupervisedDataset(Dataset):
     def __getitem__(self, i) -> Dict[str, torch.Tensor]:
         return dict(input_ids=self.input_ids[i], labels=self.labels[i])
 
+class SupervisedSKGDataset(Dataset):
+
+    def preprocess_and_tokenize(self, example, format_str, tokenizer):
+        if example["text_in"]:
+            pre_truncation = format_str.format(struct_in=example['struct_in'], text_in=example["text_in"]) + example["output"]
+        else:
+            pre_truncation = format_str.format(struct_in=example['struct_in']) + example["output"]
+
+        # get the char index of when the struct starts and ends
+        struct_start = pre_truncation.find(example['struct_in']) 
+        struct_end = struct_start + len(example['struct_in'])
+        output_start = pre_truncation.find(example['output'])
+
+        seq_in = tokenizer(pre_truncation, return_tensors="pt")
+        output_start_token = seq_in.char_to_token(output_start)
+        post_struct_token = seq_in.char_to_token(struct_end)
+        start_struct_token = seq_in.char_to_token(struct_start)
+
+        diff = max(seq_in.input_ids.shape[1] - tokenizer.model_max_length, 0)
+        
+        if post_struct_token - start_struct_token <= diff and len(example['struct_in']) > 0:
+            # if there is a struct and this is the case, we would have completely truncated the struct! 
+            # If this is the case, we should just completely throw away the example
+            return None
+        
+        struct_end_token = post_struct_token - diff
+        input_ids = torch.concat((seq_in.input_ids[0,:struct_end_token], seq_in.input_ids[0,post_struct_token:]), dim=0)
+        labels = input_ids.clone() # will modify this soon
+        # truncating the struct by an offset of diff means the output will be shifted diff tokens earlier
+        labels[:output_start_token - diff] = IGNORE_INDEX
+
+        try:
+            assert len(input_ids) == len(labels) <= tokenizer.model_max_length
+        except:
+            import pdb; pdb.set_trace()
+
+        return dict(input_ids = input_ids, labels = labels)
+
+    def __init__(self, data_path: str, has_instruction: bool, tokenizer: transformers.PreTrainedTokenizer):
+        super(SupervisedSKGDataset, self).__init__()
+        logging.warning("Loading data...")
+
+        # if there is already a tokenized data file, then just load that
+        if os.path.exists("data_v4_input_ids_labels.pkl"):
+            logging.warning("Found tokenized data file, loading...")
+            with open("data_v4_input_ids_labels.pkl", "rb") as f:
+                save = pickle.load(f)
+            self.input_ids = save["input_ids"]
+            self.labels = save["labels"]
+            return
+
+
+        list_data_dict = utils.jload(data_path)
+
+        logging.warning("Formatting and tokenizing inputs...")
+        prompt_input, prompt_no_input, prompt_no_instr = PROMPT_DICT["prompt_input"], PROMPT_DICT["prompt_no_input"], PROMPT_DICT["prompt_no_instruction"]
+        self.input_ids = []
+        self.labels = []
+        self.final_data_identifiers = []
+        for i, example in tqdm(enumerate(list_data_dict)):
+            if has_instruction:
+                format_str = prompt_input.format(instruction=example['instruction'], input=example['input_format'])
+
+                data_dict = self.preprocess_and_tokenize(example, format_str, tokenizer)
+            else:
+                # In SKG, we always have inputs, but sometimes no instructions. In Alpaca, 
+                # we may have instructions with no input but just response
+                # if we are using prompt_no_instr, we are tuning exclusively on SKG data, which always has inputs
+                format_str = prompt_no_instr.format(input=example['input_format'])
+                data_dict = self.preprocess_and_tokenize(example, format_str, tokenizer)
+            if data_dict is None:
+                # warning
+                logging.warning(f"Example {i} would have had its whole struct truncated, skipping...")
+                continue
+            self.input_ids.append(data_dict['input_ids'])
+            self.labels.append(data_dict['labels'])
+            self.final_data_identifiers.append(example['id'])
+        # dump input_ids and labels into a huge pickle file
+        save = {"input_ids": self.input_ids, "labels": self.labels}
+        with open("data_v4_input_ids_labels.pkl", "wb") as f:
+            pickle.dump(save, f)
+
+        # print the final dataset size
+        logging.warning(f"Final dataset size: {len(self.input_ids)}")
+
+        import pdb; pdb.set_trace()
+
+    def __len__(self):
+        return len(self.input_ids)
+
+    def __getitem__(self, i) -> Dict[str, torch.Tensor]:
+        return dict(input_ids=self.input_ids[i], labels=self.labels[i])
 
 @dataclass
 class DataCollatorForSupervisedDataset(object):
@@ -174,7 +277,11 @@ class DataCollatorForSupervisedDataset(object):
 
 def make_supervised_data_module(tokenizer: transformers.PreTrainedTokenizer, data_args) -> Dict:
     """Make dataset and collator for supervised fine-tuning."""
-    train_dataset = SupervisedDataset(tokenizer=tokenizer, data_path=data_args.data_path)
+    if data_args.dataset_type == "llama":
+        train_dataset = SupervisedDataset(tokenizer=tokenizer, data_path=data_args.data_path, has_instruction=data_args.has_instruction)
+    else:
+        # dataset_type = "skg"
+        train_dataset = SupervisedSKGDataset(tokenizer=tokenizer, data_path=data_args.data_path, has_instruction=data_args.has_instruction)
     data_collator = DataCollatorForSupervisedDataset(tokenizer=tokenizer)
     return dict(train_dataset=train_dataset, eval_dataset=None, data_collator=data_collator)
 
@@ -183,17 +290,12 @@ def train():
     parser = transformers.HfArgumentParser((ModelArguments, DataArguments, TrainingArguments))
     model_args, data_args, training_args = parser.parse_args_into_dataclasses()
 
-    model = transformers.AutoModelForCausalLM.from_pretrained(
-        model_args.model_name_or_path,
-        cache_dir=training_args.cache_dir,
-    )
-
     tokenizer = transformers.AutoTokenizer.from_pretrained(
         model_args.model_name_or_path,
         cache_dir=training_args.cache_dir,
         model_max_length=training_args.model_max_length,
         padding_side="right",
-        use_fast=False,
+        use_fast=True,
     )
     special_tokens_dict = dict()
     if tokenizer.pad_token is None:
@@ -205,13 +307,19 @@ def train():
     if tokenizer.unk_token is None:
         special_tokens_dict["unk_token"] = DEFAULT_UNK_TOKEN
 
+    data_module = make_supervised_data_module(tokenizer=tokenizer, data_args=data_args)
+
+    model = transformers.AutoModelForCausalLM.from_pretrained(
+        model_args.model_name_or_path,
+        cache_dir=training_args.cache_dir,
+    )
+
     smart_tokenizer_and_embedding_resize(
         special_tokens_dict=special_tokens_dict,
         tokenizer=tokenizer,
         model=model,
     )
 
-    data_module = make_supervised_data_module(tokenizer=tokenizer, data_args=data_args)
     trainer = Trainer(model=model, tokenizer=tokenizer, args=training_args, **data_module)
     trainer.train()
     trainer.save_state()
